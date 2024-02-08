b8105c2e1388488d0eabed237ec93bdf0323a8db
Sewell Stephens <sewellstephens104@gmail.com>
2024-02-08 02:12:01 +0000

    fixed applynobackground

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -21,6 +21,10 @@ function resetAndRender() {
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
 
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
+
   
 
   // do not change the below line of code
@@ -34,16 +38,77 @@ function applyAndRender() {
 // TODO 1, 2 & 4: Create the applyFilter function here
 
 
+console.log(bgPixel)
+
+function applyFilter(filterFunction) {
+
+  
+
+  for (i = 0; i < image.length; i++) {
+     for (j = 0; j < image[i].length; j++) {
+      var rgbString = image[i][j];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+     }
+  }
+
+}
+
+
 // TODO 7: Create the applyFilterNoBackground function
+
+function applyFilterNoBackground(filterFunction) {
+
+  for (i = 0; i < image.length; i++) {
+     for (j = 0; j < image[i].length; j++) {
+      if (image[i][j] !== image[0][0]) {
+        var rgbString = image[i][j];
+        var rgbNumbers = rgbStringToArray(rgbString);
+        filterFunction(rgbNumbers);
+        rgbString = rgbArrayToString(rgbNumbers);
+        image[i][j] = rgbString;
+      }
+     }
+  }
+
+}
 
 
 // TODO 5: Create the keepInBounds function
 
+function keepInBounds(param1) {
+
+  return param1 < 0 ? 0 : param1 > 255 ? 255 : param1;
+   
+}
+/*
+
+Tests:
+
+console.log(keepInBounds(-30)); // should print 0
+console.log(keepInBounds(300)); // should print 255
+console.log(keepInBounds(127)); // should print 127
+
+*/
 
 // TODO 3: Create reddify function
 
+function reddify(param1) {
+  param1[RED] = 200;
+}
+
 
 // TODO 6: Create more filter functions
+
+function decreaseBlue(param1) {
+ param1[BLUE] = keepInBounds(param1[BLUE] - 50);
+}
+
+function increaseGreenByBlue(param1) {
+  param1[GREEN] = keepInBounds(param1[BLUE] + param1[GREEN]);
+ }
 
 
 // CHALLENGE code goes below here
