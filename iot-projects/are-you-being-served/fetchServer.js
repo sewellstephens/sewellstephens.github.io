3e994e79033f3b5e2818afc25169d3261c8e3266
sewellstephens <sewellstephens104@gmail.com>
2025-02-11 19:46:23 -0600

    added test server

--- iot-projects/are-you-being-served/fetchServer.js
+++ iot-projects/are-you-being-served/fetchServer.js
@@ -1,1 +1,37 @@
 // fetchServer.js file
+const http = require('http');
+const port = 4060;
+
+
+http.createServer(async function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fetch('https://sewellstephens.github.io', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    })
+    .then(async (res1) => {
+        const fetchResponse = res1;
+        if (fetchResponse.ok) {
+           const html = await fetchResponse.text();
+           console.log(html)
+            res.write(html);
+
+           res.end()
+        //    res.writeHead(200, { "Content-Type": "text/html" }).end(html)
+        }
+        else {
+            res.write(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+            </head>
+            <body>
+                <p>${fetchResponse.status} - ${fetchResponse.statusText}</p>
+            </body>
+            </html>
+            `)
+        }
+    })
+}).listen(port);
