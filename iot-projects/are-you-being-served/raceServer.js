5679a8ac261399c33bf330573157d0d4e949e704
sewellstephens <sewellstephens104@gmail.com>
2025-02-25 18:00:08 -0600

    added server changes

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -22,7 +22,7 @@ const wrapper = (callback) => {
 }
 
     // TODO 12: Make the whole thing parallel
-    async.series( 
+    async.parallel( 
         // TODO 9: Supply an array of functions
         [
             function (callback) {
@@ -42,13 +42,17 @@ const wrapper = (callback) => {
             // TODO 10: add a callback function to the end of the async call to tally the results 
             res.write("Results:\n");
             var victoryOrder = sortTogether(racers, results);
-            res.write(racerName + "\n");
 
-            let d = new Date();
-            let endTime = d.getTime();
+            for (let i = 0; i < victoryOrder.length; i++) {
+                res.write(victoryOrder[i] + "\n");
 
-            let duration = endTime - startTime;
-            res.write("Race Duration: " + duration + "ms\n");
+                let d = new Date();
+let endTime = d.getTime();
+
+let duration = endTime - startTime;
+res.write("Race Duration: " + duration + "ms\n");
+            }
+            res.end();
         }
     );
     
