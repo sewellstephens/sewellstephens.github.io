56d93e48d73959a3450ed1bef4afc77cf3534be7
Sewell Stephens <sewellstephens104@gmail.com>
2025-05-06 18:04:33 -0500

    latest

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,5 +1,16 @@
 const express = require('express'),
-	cors = require('cors');
-	
+cors = require('cors');
+const sensorsRoutes = require('../routes/sensors.js');
+const actuatorsRoutes = require('../routes/actuators.js');
+
+const app = express();	
+app.use(cors());
+
+app.use("/pi/sensors", sensorsRoutes);
+app.use("/pi/actuators", actuatorsRoutes);
+
+app.get("/", function (req, res) {
+	res.send("the server is running");
+});
 
 module.exports = app;
